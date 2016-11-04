b8db8eb137df317b08d94f92dac279e7b8ef800f
krishnavuppe <krishnavuppe@gmail.com>
2016-11-04 14:41:12 +0100

    updated

--- Cpp QtRealtimeGraphGui/mainwindow.cpp
+++ Cpp QtRealtimeGraphGui/mainwindow.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <unistd.h>
 
+using namespace std;
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -11,6 +12,8 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    i=-1;
+    ui->clear->setDisabled(1);
 }
 
 
@@ -22,8 +25,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_Start_clicked()
 {
-    QVector<double> xaxis(2), yaxis(2);
-    int i=0;
+
     // create graph and assign data to it:
     ui->customPlot->addGraph();
     // give the axes some labels:
@@ -32,18 +34,72 @@ void MainWindow::on_Start_clicked()
     // set axes ranges, so we see all data:
     ui->customPlot->xAxis->setRange(0, 12);
     ui->customPlot->yAxis->setRange(-1, 2);
-    while(1)
-    { xaxis[0] = i*1.0;
-      if(yaxis[0]==0.0){yaxis[0] = 1.0;}
-      else{yaxis[0] = 0.0;}
-      //  ui-> customPlot->graph(0)->setData(x, y);
-      // add data to lines:
-      ui->customPlot->graph(0)->addData(xaxis[0], yaxis[0]);
-      // make key axis range scroll with the data (at a constant range size of 8):
-      if(xaxis[0] >= 12){
-      ui->customPlot->xAxis->setRange(xaxis[0], 10, Qt::AlignRight);}
-      ui->customPlot->replot();
-      i++;
-      usleep(500000);
+
+    // create graph and assign data to it:
+    ui->custom->addGraph();
+    // give the axes some labels:
+    ui->custom->xAxis->setLabel("x");
+    ui->custom->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->custom->xAxis->setRange(0, 12);
+    ui->custom->yAxis->setRange(-1, 4);
+
+if(i==-1){
+    connect(&timer, SIGNAL(timeout()), this, SLOT(update()));
+    timer.start(1000);}
+    ui->Start->setDisabled(1);
+    ui->clear->setEnabled(1);
+    i=1;
+}
+
+
+void MainWindow::on_radioButton_clicked()
+{
+    ui->custom->hide();
+    ui->customPlot->show();
+}
+
+void MainWindow::on_radioButton_2_clicked()
+{
+    ui->customPlot->hide();
+    ui->custom->show();
+}
+
+void MainWindow::on_clear_clicked()
+{
+
+    i= 0;
+    ui->customPlot->clearGraphs();
+    ui->custom->clearGraphs();
+    ui->Start->setEnabled(1);
+    ui->clear->setDisabled(1);
+}
+
+void MainWindow::update()
+{
+    if(i==0){}
+    else{
+    xaxis[0] = i*1.0;
+    if(yaxis[0]==0.0){yaxis[0] = 1.0;}
+    else{yaxis[0] = 0.0;}
+    // add data to lines:
+    ui->customPlot->graph(0)->addData(xaxis[0], yaxis[0]);
+    // make key axis range scroll with the data (at a constant range size of 8):
+    if(xaxis[0] >= 12){
+    ui->customPlot->xAxis->setRange(xaxis[0], 10, Qt::AlignRight);}
+    ui->customPlot->replot();
+    i++;
+
+    xaxis[1] = i*1.0;
+    if(yaxis[1]==0.0){yaxis[1] = 3.0;}
+    else{yaxis[1] = 0.0;}
+    // add data to lines:
+    ui->custom->graph(0)->addData(xaxis[1], yaxis[1]);
+    // make key axis range scroll with the data (at a constant range size of 10):
+    if(xaxis[1] >= 12){
+    ui->custom->xAxis->setRange(xaxis[1], 10, Qt::AlignRight);}
+    ui->custom->replot();
+    i++;
     }
 }
+
--- Cpp QtRealtimeGraphGui/mainwindow.h
+++ Cpp QtRealtimeGraphGui/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -12,13 +13,25 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    double xaxis[2], yaxis[2];
+    int i;
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     int clicked;
+    QTimer timer;
+
 
 private slots:
 
     void on_Start_clicked();
+
+    void on_clear_clicked();
+
+    void on_radioButton_clicked();
+
+    void on_radioButton_2_clicked();
+
+    void update();
 
 private:
     Ui::MainWindow *ui;
